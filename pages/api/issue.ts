5fb07eb592dadd106e9074e704f3bb8c1a91de11
AnturK <AnturK@users.noreply.github.com>
2021-08-11 11:51:42 +0200

    Completely prebaked version and switch to graphQL.

--- pages/api/issue.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-/// Fetches all relevant issues for the repository - Only required due to convention
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<number[]>
-) {
-  res.status(500).end()
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import Head from 'next/head'
-import { Octokit } from 'octokit'
-import { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+import next, { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
 import IssueDetail from './issue/[id]'
 import { useState } from 'react'
+import { request, gql } from 'graphql-request'
+
+export type IssueInfo = {
+  title: string,
+  url: string,
+  number: number
+}
 
 export default function Home({ issues }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const [chosenIssueNumber,setChosenIssueNumber] = useState(0)
+  const [chosenIssue, setChosenIssue] = useState(undefined as unknown as IssueInfo)
 
-
-  const rollRandomIssue = () =>{
-    const chosen_number = Array.from(issues).sort(() => 0.5 - Math.random()).pop() || 0;
-    setChosenIssueNumber(chosen_number)
+  const rollRandomIssue = () => {
+    const chosen = Array.from(issues).sort(() => 0.5 - Math.random()).pop() || undefined;
+    if (chosen !== undefined)
+      setChosenIssue(chosen)
   }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
@@ -28,37 +34,76 @@ export default function Home({ issues }: InferGetStaticPropsType<typeof getStati
           <button className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onClick={rollRandomIssue}>Roll</button>
         </div>
         <div className="">
-          {chosenIssueNumber !== 0 && <IssueDetail id={chosenIssueNumber}/>}
+          {chosenIssue !== undefined && <IssueDetail issue={chosenIssue} />}
         </div>
       </main>
     </div>
   )
 }
 
-export const getStaticProps = async(context : GetStaticPropsContext) => {
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   //All issue list refreshed every 30 minutes.
   const default_repo = process.env.GITHUB_REPO || "tgstation"
-  const default_owner = process.env.GITHUB_REPO_OWNER  || "tgstation"
+  const default_owner = process.env.GITHUB_REPO_OWNER || "tgstation"
   const api_key = process.env.GITHUB_API_KEY || "Missing API key"
-  
-  const octokit = new Octokit({auth:api_key});
 
-  const all_valid_issues = await octokit.paginate(
-    "GET /repos/{owner}/{repo}/issues",
-    {
-      owner: default_owner,
-      repo: default_repo,
-      state: 'open',
-      labels: "Bug",
-      per_page: 100,
-    },
-    (response) => response.data.map((issue) => issue.number)
-  );
+  const endpoint = 'https://api.github.com/graphql'
+
+  const query = gql`
+  query GetIssues($page_cursor: String){
+    repository(owner:"${default_owner}", name:"${default_repo}") {
+      issues(states:OPEN,first:100,labels:"Bug",after:$page_cursor) {
+        pageInfo{
+          hasNextPage
+          endCursor
+        }
+        edges {
+          node {
+            title
+            url
+            number
+          }
+        }
+      }
+    }
+  }`
+
+  type QueryData = {
+    repository: {
+      issues: {
+        pageInfo: {
+          hasNextPage: boolean,
+          endCursor: string
+        }
+        edges: {
+          node: IssueInfo
+        }[]
+      }
+    }
+  }
+
+  const requestHeaders = {
+    authorization: `bearer ${api_key}`
+  }
+
+  var filtered = [] as IssueInfo[]
+  var has_next_page = false;
+  var cursor = null as unknown as string;
+  do {
+    const variables = {
+      page_cursor: cursor
+    }
+    const result = await request<QueryData>(endpoint, query, variables, requestHeaders)
+    const data = result.repository.issues.edges.map(x => x.node)
+    filtered.push(...data)
+    has_next_page = result.repository.issues.pageInfo.hasNextPage
+    cursor = result.repository.issues.pageInfo.endCursor
+  } while (has_next_page)
 
   return {
-    props :{
-      issues : all_valid_issues
+    props: {
+      issues: filtered
     },
-    revalidate: 30*60 //every 30 minutes
+    revalidate: 30 * 60 //every 30 minutes
   }
 }
